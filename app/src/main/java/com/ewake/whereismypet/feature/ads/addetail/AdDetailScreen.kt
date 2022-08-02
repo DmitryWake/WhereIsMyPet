25629df2ebcc6c8014c2c31ac51754dd4f2d0056
Dmitry_Wake <nikolaevskiy2001@gmail.com>
2022-08-02 12:08:46 +0300

    base ad detail screen

--- app/src/main/java/com/ewake/whereismypet/feature/ads/addetail/AdDetailScreen.kt
+++ app/src/main/java/com/ewake/whereismypet/feature/ads/addetail/AdDetailScreen.kt
@@ -1,12 +1,30 @@
 package com.ewake.whereismypet.feature.ads.addetail
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.SubcomposeAsyncImage
+import com.ewake.whereismypet.core.model.AdModel
 import com.ewake.whereismypet.feature.ads.addetail.viewmodel.AdDetailScreenUiState
 import com.ewake.whereismypet.feature.ads.addetail.viewmodel.AdDetailViewModel
 import com.ewake.whereismypet.feature.ads.addetail.viewmodel.AdModelUiState
@@ -19,9 +37,60 @@ import com.ewake.whereismypet.feature.ads.addetail.viewmodel.AdModelUiState
 fun AdDetailScreen(viewModel: AdDetailViewModel = hiltViewModel()) {
     val uiState: AdDetailScreenUiState by viewModel.uiState.collectAsStateWithLifecycle()
 
-    when (uiState.adModelUiState) {
-        AdModelUiState.Error -> Text(text = "Ошибка")
-        AdModelUiState.Loading -> CircularProgressIndicator()
-        is AdModelUiState.Success -> Text(text = (uiState.adModelUiState as AdModelUiState.Success).data.id)
+    Scaffold(
+        topBar = {
+            AdDetailAppBar()
+        }
+    ) {
+        when (uiState.adModelUiState) {
+            AdModelUiState.Error -> Text(text = "Ошибка")
+            AdModelUiState.Loading -> CircularProgressIndicator(modifier = Modifier.fillMaxSize())
+            is AdModelUiState.Success -> AdDetailSuccess(model = (uiState.adModelUiState as AdModelUiState.Success).data)
+        }
+    }
+}
+
+@Composable
+private fun AdDetailAppBar() {
+    TopAppBar(
+        title = {
+            Text(text = "Объявление")
+        },
+        navigationIcon = {
+            IconButton(onClick = { }) {
+                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back")
+            }
+        },
+        backgroundColor = Color.Transparent,
+        elevation = 2.dp
+    )
+}
+
+@Composable
+private fun AdDetailSuccess(model: AdModel) {
+    Column {
+        SubcomposeAsyncImage(
+            model = model.iconUrl,
+            contentDescription = model.title,
+            loading = {
+                CircularProgressIndicator()
+            },
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(16f / 9f)
+        )
+
+        Text(
+            text = model.title,
+            fontSize = 32.sp,
+            modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = model.description,
+            fontSize = 18.sp,
+            modifier = Modifier.padding(start = 16.dp, bottom = 16.dp, end = 16.dp),
+            color = Color.Gray
+        )
     }
 }
